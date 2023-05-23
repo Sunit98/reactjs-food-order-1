1252d97a495435920db19986ced50478f50b9d1d
Sunit Rawat <rawatsunit5@gmail.com>
2023-05-23 15:55:58 +0530

    adding cart feature again

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={showCartHandler} />}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
